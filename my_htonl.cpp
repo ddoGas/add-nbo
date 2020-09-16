5ced581b71a73e562bda253ee4ab1b16d5123199
ddoGas <ddogas101@gmail.com>
2020-09-16 18:50:09 +0900

    added function comment

--- my_htonl.cpp
+++ my_htonl.cpp
@@ -2,7 +2,7 @@
 #include "my_htonl.h"
 
 uint32_t my_htonl(uint32_t n){
-    /**/
+	//works same as htonl
 	uint32_t n1 = (n & 0xff000000)>>24;
 	uint32_t n2 = (n & 0x00ff0000)>>8;
 	uint32_t n3 = (n & 0x0000ff00)<<8;
